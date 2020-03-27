93507afa9bb2d7aafe38d4ae59d3dff44aae20f4
Skillfun9 <mequinho03@gmail.com>
2020-03-27 19:06:14 +0000

    backend update + Moloni Route/Controller

--- backendMequinho/controllers/boltMoloni.controller.js
+++ backendMequinho/controllers/boltMoloni.controller.js
@@ -46,7 +46,7 @@ qs: { access_token: '' + moloni_access_token },
     reference: req.body.reference
     }
 };
-
+ var data={};
 request(options, function(error, response, body) {
   if (error) throw new Error(error);
    //res.status(200).send(body);
@@ -54,14 +54,21 @@ request(options, function(error, response, body) {
     for (var i = 0; i < packages_data.length; i++) {
     packages.push(packages_data[i].package_id);
     }
-    console.log(packages_data);
-    console.log(packages);
-    //converte para string para aparecer no postman
-    res.end(JSON.stringify(packages_data));
+    /*
+       console.log(packages_data);
+       console.log(packages);
+       //converte para string para aparecer no postman
+       res.end(JSON.stringify(packages_data));
+    */
 
+         data.packages = {};
+         data.packages = packages_data;
+         res.end(JSON.stringify(data));
+         console.log(data);
+         console.log(packages_data);
     });
 
-console.log(packages);
+
 
 
 }
