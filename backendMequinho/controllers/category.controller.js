a44d1a04d14abeada326a060899c8d785a3c42a5
Skillfun9 <mequinho03@gmail.com>
2020-03-27 18:24:29 +0000

    backend update + Moloni Route/Controller

--- backendMequinho/controllers/category.controller.js
+++ backendMequinho/controllers/category.controller.js
@@ -4,8 +4,8 @@ var moloni_client_secret = '125463ae6755e5308d9f6b86728022805d1582ec';
 var request = require('request');
 var express = require('express');
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var packages = [];
-var packages_data;
+var categories = [];
+var category_data;
 
 
 function getToken() {
@@ -24,7 +24,7 @@ console.log("moloni_access_token: " + moloni_access_token + "   moloni_refresh_t
 
 }
 
-function getPackages(req, res) {
+function getCategory(req, res) {
 
 getToken();
 console.log("Getting acess_token");
@@ -32,7 +32,7 @@ console.log("Getting acess_token");
  var options = {
 
 method: 'POST',
-url: 'https://api.moloni.pt/v1/products/getAll/',
+url: 'https://api.moloni.pt/v1/productCategories/getAll/',
 qs: { access_token: '' + moloni_access_token },
   headers: {
    'cache-control': 'no-cache',
@@ -41,29 +41,27 @@ qs: { access_token: '' + moloni_access_token },
 
     form: {
     company_id: '127896',
-    category_id: '2183110',
-    name: req.body.name,
-    reference: req.body.reference
+    parent_id: '0'
     }
 };
 
 request(options, function(error, response, body) {
   if (error) throw new Error(error);
    //res.status(200).send(body);
-    var packages_data = JSON.parse(response.body);
-    for (var i = 0; i < packages_data.length; i++) {
-    packages.push(packages_data[i].package_id);
+    var category_data = JSON.parse(response.body);
+    for (var i = 0; i < category_data.length; i++) {
+    categories.push(category_data[i].category_id);
     }
-    console.log(packages_data);
-    console.log(packages);
+    console.log(category_data);
+    console.log(categories);
 
     });
 
-console.log(packages);
+console.log(categories);
 
 }
 
 //exportar as funções
 module.exports = {
-    getPackages: getPackages
+    getCategory: getCategory
    };
