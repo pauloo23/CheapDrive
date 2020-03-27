fa69fef8e4452e715feaf444156ef464537db075
antoniopt4 <43757497+antoniopt4@users.noreply.github.com>
2020-03-27 23:24:03 +0000

    Update main.route.js

--- backendMequinho/routes/main.route.js
+++ backendMequinho/routes/main.route.js
@@ -3,19 +3,15 @@ const router =require('express').Router();
 const controllerBolt = require('../controllers/boltMoloni.controller.js');
 const controllerCompany = require('../controllers/company.controller.js');
 const controllerCategory = require('../controllers/category.controller.js');
-
+const controllerUber = require('../controllers/uberJasmin.controller.js');
 
 //Bolt using moloni
 router.get('/bolt/', controllerBolt.getPackages);
 router.get('/company/', controllerCompany.getCompany);
 router.get('/category/', controllerCategory.getCategory);
 
-/*
-router.get('/bolt/:id', controllerBolt.readID);
-router.post('/bolt/', controllerBolt.save);
-router.put('/bolt/:id', controllerBolt.update);
-router.delete('/bolt/:id', controllerBolt.deleteID);
-*/
+//Uber using Jasmin
+router.get('/uber/', controllerUber.getPacotes);
 
 
 module.exports = router;
