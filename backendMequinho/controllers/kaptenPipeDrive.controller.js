404e5ba9e8a82843485f2841939cc3c87ea25140
Rui <Rui@DESKTOP-8VHSQ7F>
2020-04-15 23:03:50 +0100

    Ligação feita

--- backendMequinho/controllers/kaptenPipeDrive.controller.js
+++ backendMequinho/controllers/kaptenPipeDrive.controller.js
@@ -2,47 +2,17 @@ var request = require('request');
 
 function getPackages(req, res) {
 
-console.log("Init getPackages function...")
-
-
-
-         var options = {
-             method: 'GET',
-             url:'https://api.pipedrive.com/v1/products?start=0&api_token=1342c3a366d4a54c321ea28bad40b488699c658c',
-
-
-        headers:
-     { 'content-type': 'application/json' },
-
-        };
-var data = {};
-
-
-     request(options, function (error, response, body) {
-         if (error) throw new Error(error);
-         //res.status(200).send(body);
-
-         var oi = JSON.parse(response.body);
-
-         var dados = oi.data;
-         var nomes = [];
-         var code = [];
-
-
-         for (var i = 0; i < dados.length; i++) {
-                nomes.push(dados[i].name);
-                code.push(dados[i].code);
-
-
-                console.log(nomes);
-                console.log(code);
-    }
-    var oi = {};
-            oi.nomes= nomes;
-            oi.code= code;
-
-
-     });
+    console.log("Init getPackages function...")
+    var options = {
+        method: 'GET',
+        url:'https://api.pipedrive.com/v1/products?start=0&api_token=1342c3a366d4a54c321ea28bad40b488699c658c',
+        headers: { 'content-type': 'application/json' },
+    };
+    var data = {};
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+        res.end(response.body);
+    });
 
 
 }
