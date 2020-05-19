8a2fd6cc0938bdb5115f451a1f47c7fab72ed855
Skillfun9 <mequinho03@gmail.com>
2020-05-19 17:07:20 +0100

    paypal

--- backendMequinho/routes/main.route.js
+++ backendMequinho/routes/main.route.js
@@ -5,6 +5,7 @@ const controllerCompany = require('../controllers/company.controller.js');
 const controllerCategory = require('../controllers/category.controller.js');
 const controllerUber = require('../controllers/uberJasmin.controller.js');
 const controllerKapten = require('../controllers/kaptenPipeDrive.controller.js');
+const controllerPaypal = require('../controllers/paypalTeste.controller.js');
 
 //Bolt using moloni
 router.get('/bolt/', controllerBolt.getPackages);
@@ -16,6 +17,11 @@ router.get('/uber/', controllerUber.getPacotes);
 
 //Kapten using Pipedrive
 router.get('/kapten/', controllerKapten.getPackages);
+
+//Paypal payment
+router.get('/paypal/', controllerPaypal.getPaypal);
+router.post('/paypal/', controllerPaypal.postPaypal);
+
 
 
 module.exports = router;
