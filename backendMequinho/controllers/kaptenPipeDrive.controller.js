7b04cbea8aace47f899cdd423b568a23c662b1dc
Rui <Rui@DESKTOP-8VHSQ7F>
2020-06-03 15:42:18 +0100

    Kapten Controller

--- backendMequinho/controllers/kaptenPipeDrive.controller.js
+++ backendMequinho/controllers/kaptenPipeDrive.controller.js
@@ -5,7 +5,7 @@ function getPackages(req, res) {
     console.log("Init getPackages function...")
     var options = {
         method: 'GET',
-        url:'https://api.pipedrive.com/v1/products?start=0&api_token=6abe6de80e6a85e62b0bdb1bb8021c58f94223f5',
+        url:'https://api.pipedrive.com/v1/products?start=0&api_token=c8b64dfd1d3cc8e532203f76ff81055eac5de6e1',
         headers: { 'content-type': 'application/json' },
     };
     var data = {};
