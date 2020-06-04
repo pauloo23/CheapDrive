244780d12a415e976149eb206f26420a4eecb69d
antonipt4 <antoniopt4@gmail.com>
2020-06-04 13:51:07 +0100

    .

--- backendMequinho/controllers/user.controller.js
+++ backendMequinho/controllers/user.controller.js
@@ -28,45 +28,10 @@ connect.getConnection(function (err, conn) {
 
 
 
-module.exports = {
-    read : read
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-function read(req, res) {
-
-    connect.con.query('SELECT * FROM users order by id', function(err, rows, fields) {
-        if (err) {
-            console.log(err);
-            res.send(err);
-        }
-        else {
-            if (rows.length == 0) {
-                res.send("erro");
-            }
-            else {
-                res.send(rows);
-            }
-        }
-    });
-}
-*/
-
 //Export functions
 module.exports = {
 
     read: read
+
 };
 
