ab65916c53caa7b9b09fd3180eac3f983376d389
antoniopt4 <43757497+antoniopt4@users.noreply.github.com>
2020-06-07 16:32:52 +0100

    Update user.controller.js

--- backendMequinho/controllers/user.controller.js
+++ backendMequinho/controllers/user.controller.js
@@ -24,6 +24,103 @@ connect.getConnection(function (err, conn) {
 });}
 
 
+function readC(req, res) {
+    connect.getConnection(function (err, conn) {
+        if (err)
+            return res.send(400);
+
+        // if you got a connection...
+        conn.query('SELECT * FROM cliques ', function(err, rows) {
+            if(err) {
+                conn.release();
+
+
+            }
+
+            // for simplicity, just send the rows
+            res.send(rows);
+
+            // CLOSE THE CONNECTION
+            conn.release();
+        })
+    });}
+
+
+function saveU(req, res) {
+    var opiniao = {
+        empresa: "Uber",
+
+    };
+    var insert_query = 'INSERT INTO cliques SET ?';
+    connect.getConnection(function (err, conn) {
+        if (err)
+            return res.send(400);
+
+
+
+        // if you got a connection...
+        conn.query(insert_query, opiniao, function(err, rows, fields) {
+            if (err) throw err
+            console.log('Comentário inserido com sucesso!');
+        });
+
+        res.send('Comentário inserido com sucesso!');
+        // CLOSE THE CONNECTION
+        conn.release();
+    })
+};
+
+function saveK(req, res) {
+    var opiniao = {
+        empresa: "Kapten",
+
+    };
+    var insert_query = 'INSERT INTO cliques SET ?';
+    connect.getConnection(function (err, conn) {
+        if (err)
+            return res.send(400);
+
+
+
+        // if you got a connection...
+        conn.query(insert_query, opiniao, function(err, rows, fields) {
+            if (err) throw err
+            console.log('Comentário inserido com sucesso!');
+        });
+
+        res.send('Comentário inserido com sucesso!');
+        // CLOSE THE CONNECTION
+        conn.release();
+    })
+};
+
+
+function saveB(req, res) {
+    var opiniao = {
+        empresa: "Bolt",
+
+    };
+    var insert_query = 'INSERT INTO cliques SET ?';
+    connect.getConnection(function (err, conn) {
+        if (err)
+            return res.send(400);
+
+
+
+        // if you got a connection...
+        conn.query(insert_query, opiniao, function(err, rows, fields) {
+            if (err) throw err
+            console.log('Comentário inserido com sucesso!');
+        });
+
+        res.send('Comentário inserido com sucesso!');
+        // CLOSE THE CONNECTION
+        conn.release();
+    })
+};
+
+
+
 
 
 
@@ -31,7 +128,12 @@ connect.getConnection(function (err, conn) {
 //Export functions
 module.exports = {
 
-    read: read
+    read: read,
+    readC: readC,
+    saveU: saveU,
+    saveB: saveB,
+    saveK: saveK
 
 };
+
 
